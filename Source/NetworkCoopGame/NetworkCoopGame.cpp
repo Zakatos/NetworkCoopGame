4845934f2b82eb58ce0040945819563fcd716ec6
Zakatos <ameletiadis@yahoo.com>
2019-05-21 22:03:03 +0200

    Initial commit

---
+++ Source/NetworkCoopGame/NetworkCoopGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NetworkCoopGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NetworkCoopGame, "NetworkCoopGame" );
---
+++ Source/NetworkCoopGame/NetworkCoopGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetworkCoopGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKCOOPGAME_API ANetworkCoopGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
